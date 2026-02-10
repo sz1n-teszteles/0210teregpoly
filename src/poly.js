12f1a475b289e9ca1f665c3656953da5d8bfb95c
kala <kalae1337@gmail.com>
2026-02-10 15:10:06 +0100

    fixed

--- src/poly.js
+++ src/poly.js
@@ -1,6 +1,12 @@
 
 
 function calcArea(sideLength, numSides) {
+    if (sideLength <= 0 || numSides <= 2){
+        throw new Error('Hiba!')
+    }
+    if(sideLength > 10000000 || numSides > 10000000) {
+        throw new Error('Hiba!')
+    }
     return (numSides * Math.pow(sideLength, 2)) 
     / (4 * Math.tan(Math.PI/numSides))
 }
--- test/poly.spec.js
+++ test/poly.spec.js
@@ -1,33 +1,39 @@
-import {expect} from 'chai'
+/*
+* File: poly.spec.js
+* Author: Dóra Boglárka
+* Copyright: 2026, Dóra Boglárka
+* Group: Sz I N
+* Date: 2026-02-10
+* Github: https://github.com/kalae1337
+* Licenc: MIT
+*/
+
+import { expect } from 'chai'
 import { calcArea } from '../src/poly.js'
 
 describe('calcArea() fuggveny tesztelese', () => {
-    it('in: 2, 5; out: 6.88', () => {
-        const expected = calcArea(30,3)
-        expect(expected).to.closeTo(389.71, 0.1)
+    it('in: 30, 3; out: 389.71', () => {
+        const expected = calcArea(30, 3)
+        expect(expected).closeTo(389.71, 0.1)
     })
     it('in: 135, 30; out: 1300494.69', () => {
-        const expected = calcArea(135, 30)
-        expect(expected).to.closeTo(1300494.69, 0.1)
+        const result = calcArea(135, 30)
+        expect(result).closeTo(1300494.69, 0.1)
     })
     it('0,30 hiba', () => {
-        expect(() => calcArea(0,30).throw(Error))
+        expect(() => calcArea(0,30).to.throw(Error))
     })
     it('in: 30, 2; out: hiba', () => {
-        const expected = calcArea(30,2)
-        expect(expected).to.throw
+        expect(() => calcArea(30,2).to.throw(Error))
     })
     it('in: 30, 0; out: hiba', () => {
-        const expected = calcArea(30,0)
-        expect(expected).to.throw
+        expect(() => calcArea(30,0).to.throw(Error))
     })
     it('in: 10000001, 3; out: hiba', () => {
-        const expected = calcArea(10000001, 3)
-        expect(expected).to.throw
+        expect(() => calcArea(10000001,3).to.throw(Error))
     })
     it('in: 30, 10000001; out: hiba', () => {
-        const expected = calcArea(30, 10000001)
-        expect(expected).to.throw
+        expect(() => calcArea(30, 10000001).to.throw(Error))
     })
     
 })
